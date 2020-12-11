8c3faab310bb0ab3014add3884367ec86c389794
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2020-12-11 15:49:10 -0500

    ci: Move visual-diff to github actions

--- test/perceptual/d2l-grade-result-custom-manual-override-text.visual-diff.js
+++ test/perceptual/d2l-grade-result-custom-manual-override-text.visual-diff.js
@@ -12,7 +12,6 @@ describe('optional override text substitution visual diff tests', () => {
 	before(async() => {
 		browser = await puppeteer.launch();
 		page = await browser.newPage();
-		await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
 		await page.goto(`${visualDiff.getBaseUrl()}/test/perceptual/d2l-grade-result-custom-manual-override-text.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
 		await page.bringToFront();
 		await visualDiff.disableAnimations(page);
